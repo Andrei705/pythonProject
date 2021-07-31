2e95ed0464a9a264dac221e56f031ce11ceec0f5
Andrei705 <75162990+Andrei705@users.noreply.github.com>
2021-07-31 23:31:16 +0300

    Дополнил

--- venv/mysite/polls/models.py
+++ venv/mysite/polls/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
+class Survey(models.Model):
     title = models.CharField('Название', max_length=250)
-    start_date = models.DateField('Дата начала')
-    stop_date = models.DateField('Дата окончания')
+    start_date = models.DateTimeField('Дата начала')
+    stop_date = models.DateTimeField('Дата окончания')
     description = models.TextField('Описание')
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete= models.CASCADE)
+    text_question = models.TextField('Текст вопроса')
+    type_question = models.TextField('Тип вопроса', help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
 
     def __str__(self):
         return  self.title
 
 
     class Meta:
-        verbose_name = 'Опрос   '
+        verbose_name = "Опрос"
