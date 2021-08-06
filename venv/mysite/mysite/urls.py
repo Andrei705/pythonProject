9fa3188ef21f3e42b97fe23e87802134567d305a
Andrei705 <75162990+Andrei705@users.noreply.github.com>
2021-08-06 10:14:06 +0300

    Добавил в mysite/urls

--- venv/mysite/mysite/urls.py
+++ venv/mysite/mysite/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polls.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('polls.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(setting.POLLS_URL, documen_root=settings_POLLS_ROOT)
