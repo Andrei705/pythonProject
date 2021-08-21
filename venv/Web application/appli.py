d7489aa5fb40675095ba39e116ae64aeff2ba3fa
Andrei705 <75162990+Andrei705@users.noreply.github.com>
2021-08-21 08:40:37 +0300

    работа с Flask(добавил форму для заполнения бд)

--- venv/Web application/appli.py
+++ venv/Web application/appli.py
@@ -1,27 +1,15 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import db, Type
 
 
 app = Flask(__name__)
 app.debug =True
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123456@localhost:5432/object_db"
 app.config['SQLALCHEMY_TRACK_MODIFYCATIONS']= False
-db = SQLAlchemy(app)
 
 db.init_app(app)
 migrate = Migrate(db, app)
-
-class Type(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    folder = db.Column(db.Text, nullable=False)
-    type_one = db.Column(db.Text, nullable=False)
-    type_two = db.Column(db.Text, nullable=False)
-
-
-    def __repr__(self):
-        return 'Type %r' % self.folder
 
 
 
@@ -31,9 +19,6 @@ def form():
 
 @app.route('/completion', methods =['POST', 'GET'])
 def completion():
-    if request.method == "GET":
-        return "Error"
-
     if request.method == "POST":
         folder = request.form['folder']
         type_one = request.form['type_one']
@@ -41,7 +26,7 @@ def completion():
         completion_db = Type(folder=folder, type_one=type_one, type_two=type_two)
         db.session.add(completion_db)
         db.session.commit()
-        return f'Ok'
+        return 'OK'
 
 
 if __name__ == "__main__":
--- venv/Web application/models.py
+++ venv/Web application/models.py
@@ -1,3 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Type(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    folder = db.Column(db.String(100), nullable=False)
+    type_one = db.Column(db.String(100), nullable=False)
+    type_two = db.Column(db.String(100), nullable=False)
 
 
-
+    def __repr__(self):
+        return '<Type %r>' % self.id
