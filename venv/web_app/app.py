a28da8eaa0d2fbff339acb03632241c83ec87caa
Andrei705 <75162990+Andrei705@users.noreply.github.com>
2021-09-06 14:37:10 +0300

    работа с Flask(реализация прав доступа)

--- venv/web_app/app.py
+++ venv/web_app/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, abort
 from flask_migrate import Migrate
-from models import db, Type
+from models import db, Type, login
 
 app = Flask(__name__)
 app.debug =False
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123456@localhost:5432/object_db"
 app.config['SQLALCHEMY_TRACK_MODIFYCATIONS']= False
 
+login.login_view = 'login'
+login.init_app(app)
 db.init_app(app)
 migrate = Migrate(db, app)
+
 
 @app.route('/object/', defaults={'id':''})
 @app.route('/object/<int:id>/')
--- venv/web_app/models.py
+++ venv/web_app/models.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
 
+login = LoginManager()
 db = SQLAlchemy()
 
 class Type(db.Model):
@@ -13,12 +16,24 @@ class Type(db.Model):
         return '<Type {}>'.format(self.id)
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     user_name = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+
+    def gen_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
         return '<{}:{}>'.format(self.id, self.user_name)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
