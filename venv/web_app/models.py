ac9c97b24c982edb6615e16a598583cbc828490f
Andrei705 <75162990+Andrei705@users.noreply.github.com>
2021-10-19 13:55:53 +0300

    добавил readme

--- venv/web_app/models.py
+++ venv/web_app/models.py
@@ -1,8 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
-
-from string import whitespace
 
 login = LoginManager()
 db = SQLAlchemy()
@@ -15,25 +11,3 @@ class Type(db.Model):
 
     def __repr__(self):
         return '<Type {}>'.format(self.id)
-
-#  Создание пользователя с хеширование пароля
-# class User(UserMixin,db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     password_hash = db.Column(db.String(100), nullable=False)
-#     email = db.Column(db.String(100), nullable=False, unique=True)
-#
-#
-#     def gen_password(self,password):
-#         self.password_hash = generate_password_hash(password)
-#
-#
-#     def check_password(self,password):
-#         return check_password_hash(self.password_hash, password)
-#
-#
-#     def __repr__(self):
-#         return '<{}:{}>'.format(self.id, self.user_name)
-#
-# @login.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
